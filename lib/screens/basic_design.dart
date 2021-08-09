815913149fa28489a00c1652c397c449d841dd85
Edri94 <edri.alan.94@gmail.com>
2021-08-09 10:47:17 -0500

    termino primer diseno
    termino priemr diseño

--- lib/screens/basic_design.dart
+++ lib/screens/basic_design.dart
@@ -11,7 +11,15 @@ class BasicDesignScreen extends StatelessWidget {
           image: AssetImage('assets/landscape.jpg'),
         ),
         //Titulo
-        Title()
+        Title(),
+        //Button section
+        ButtonSection(),
+        //Description
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+          child: Text(
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
+        )
       ],
     ));
   }
@@ -50,6 +58,51 @@ class Title extends StatelessWidget {
           Text('41')
         ],
       ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          CustomBotton(icon: Icons.call, text: 'Call'),
+          CustomBotton(icon: Icons.router, text: 'Route'),
+          CustomBotton(icon: Icons.share, text: 'Share')
+        ],
+      ),
+    );
+  }
+}
+
+class CustomBotton extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  const CustomBotton({
+    Key? key,
+    required this.icon,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(
+          this.icon,
+          color: Colors.blue,
+        ),
+        Text(this.text)
+      ],
     );
   }
 }
